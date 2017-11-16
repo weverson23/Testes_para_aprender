64f3847a361d2cbd58ff60ef22b5580033e0dca3
Weverson Oliveira <weversonoliveira12@gmail.com>
2017-11-16 16:39:38 -0200

    Treino

---
+++ Teste02/src/teste02/Teste02.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teste02;
+
+/**
+ *
+ * @author wev
+ */
+public class Teste02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Mais uma ves pra ver se aprende essa merda");
+    }
+    
+}
